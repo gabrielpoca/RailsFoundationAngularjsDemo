2abf29cc46e7d6dcadfc66ac0b4f49bc171d0d14
Gabriel Poça <gabrielpoca@gmail.com>
2012-10-27 19:06:59 +0100

    added comments to posts and automatic view update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,24 @@
 class CommentsController < ApplicationController
 
+  def index
+    @comments = Comment.find_all_by_post_id(params[:post_id])
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @comments }
+    end
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+    respond_to do |format|
+      format.json { render json: @comment }
+    end
+  end
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment])
-    redirect_to post_path(@post)
+    redirect_to post_comment_path(@post.id, @comment.id)
   end
 
   def destroy
